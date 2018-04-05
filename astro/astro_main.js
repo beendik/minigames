9ca520374623988d79fe0d1e777108abc2292d53
Claus Martinsen <clauswrm@gmail.com>
2018-04-05 20:47:50 +0200

    Made player rotate toward mouse. Changed Player to a triangle.

--- astro/astro_main.js
+++ astro/astro_main.js
@@ -3,23 +3,32 @@ var context = canvas.getContext('2d');
 
 function play() {
     var player = new Player(canvas.width / 2, canvas.height / 2);
-    var LEFT, RIGHT, UP, DOWN;
+    var left, right, up, down;
+    var mousePos = {
+        x: 0,
+        y: 0
+    };
 
     document.onkeydown = function (e) {
-        if (e.keyCode === 37 || e.keyCode === 65) LEFT = true;
-        if (e.keyCode === 38 || e.keyCode === 87) UP = true;
-        if (e.keyCode === 39 || e.keyCode === 68) RIGHT = true;
-        if (e.keyCode === 40 || e.keyCode === 83) DOWN = true;
+        if (e.keyCode === 37 || e.keyCode === 65) left = true;
+        if (e.keyCode === 38 || e.keyCode === 87) up = true;
+        if (e.keyCode === 39 || e.keyCode === 68) right = true;
+        if (e.keyCode === 40 || e.keyCode === 83) down = true;
     };
 
     document.onkeyup = function (e) {
-        if (e.keyCode === 37 || e.keyCode === 65) LEFT = false;
-        if (e.keyCode === 38 || e.keyCode === 87) UP = false;
-        if (e.keyCode === 39 || e.keyCode === 68) RIGHT = false;
-        if (e.keyCode === 40 || e.keyCode === 83) DOWN = false;
+        if (e.keyCode === 37 || e.keyCode === 65) left = false;
+        if (e.keyCode === 38 || e.keyCode === 87) up = false;
+        if (e.keyCode === 39 || e.keyCode === 68) right = false;
+        if (e.keyCode === 40 || e.keyCode === 83) down = false;
+    };
+
+    document.onmousemove = function (e) {
+        mousePos = getRelativeMousePos(e)
     };
 
     function draw() {
+        // Main loop
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         player.updatePos();
@@ -27,27 +36,31 @@ function play() {
         window.requestAnimationFrame(draw)
     }
 
+    // Start animating
     window.requestAnimationFrame(draw);
 
     function Player(x, y) {
+        // Position
         this.x = x;
         this.y = y;
+        // Velocity
         this.dx = 0;
         this.dy = 0;
+        // Acceleration constants
         this.acceleration = 0.5;
         this.drag_coefficient = 0.02;
 
         this.updatePos = function () {
-            if (LEFT) {
+            if (left) {
                 this.dx -= this.acceleration;
             }
-            if (RIGHT) {
+            if (right) {
                 this.dx += this.acceleration;
             }
-            if (UP) {
+            if (up) {
                 this.dy -= this.acceleration;
             }
-            if (DOWN) {
+            if (down) {
                 this.dy += this.acceleration;
             }
 
@@ -80,21 +93,32 @@ function play() {
             context.save();
             context.fillStyle = "#ff1f6d";
             context.translate(this.x, this.y);
-            context.rotate(Math.PI / 4);
-            context.fillRect(0, 0, 10, 10);
+
+            // Rotate toward mouse position
+            context.rotate(getAngle(this.x, this.y, mousePos.x, mousePos.y));
+
+            // Draw player
+            context.beginPath();
+            context.moveTo(0, -12);
+            context.lineTo(-12, 12);
+            context.lineTo(12, 12);
+            context.fill();
             context.restore();
         };
     }
 
-    function Projectile(owner, x, y, speed) {
-        this.owner = owner;
-    }
-
-    function getMousePos(canvas, evt) {
+    function getRelativeMousePos(e) {
         var rect = canvas.getBoundingClientRect();
         return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top
         };
+    }
+
+    function getAngle(x1, y1, x2, y2) {
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+
+        return Math.atan2(dx, dy) * -1 + Math.PI;
     }
 }
