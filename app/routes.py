1351405f73c3a823b2e608975863d39cda8c10b3
Bendik Schrøder <bendik96@gmail.com>
2018-04-08 22:22:00 +0200

    Experimenting with 404s

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,4 @@
-from flask import render_template
-from flask import url_for
+from flask import render_template, abort
 from app import flaskInstance
 from app.games import games
 
@@ -12,3 +11,14 @@ def index():
 def game(game):
     if game in games:
         return render_template('game.html', game=games[game])
+    else:
+        return render_template('404.html', word=game, route='game'), 404
+
+
+def gameNotExisting(game):
+    return render_template('404.html', word=game)
+
+
+@flaskInstance.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', word=e), 404
