27115922c0cc287d9345b9c726f96d8bd3efd81a
Bendik Schrøder <bendik96@gmail.com>
2018-04-08 23:31:03 +0200

    Better game js naming

--- app/games.py
+++ app/games.py
@@ -3,14 +3,14 @@ games = {
         'title': 'Astro',
         'description': 'Fly med WASD eller piltaster \u2190 \u2191 \u2192 \u2193 og skyt med musen \U0001F5B1',
         'thumbnail': 'path/to/thumb',
-        'script': 'js/astro_main.js',
+        'script': 'js/astro.js',
         'freezeFrame': 'path/to/freezeframe'
     },
     'flap': {
         'title': 'Flap',
         'description': 'Hopp med klikk eller space.',
         'thumbnail': 'path/to/thumb',
-        'script': 'js/main.js',
+        'script': 'js/flap.js',
         'freezeFrame': 'path/to/freezeframe'
     }
 }
